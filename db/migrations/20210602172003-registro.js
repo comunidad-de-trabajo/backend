672b24044a3b460c772561503be2ce668492b4d1
kevinmatias120 <acastakev888@gmail.com>
2021-06-02 19:14:17 -0300

    paginacion y CRUD registro de datos OK

--- db/migrations/20210602172003-registro.js
+++ db/migrations/20210602172003-registro.js
@@ -8,15 +8,22 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+<<<<<<< HEAD
       /*otros datos*/
       estado: {
         type: Sequelize.STRING,
       },
+=======
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
       /*Dato-Empresa*/
       nombreComercial: {
         type: Sequelize.STRING,
       },
+<<<<<<< HEAD
       provinciaActual: {
+=======
+      provincia: {
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         type: Sequelize.STRING,
       },
       localidad: {
@@ -32,22 +39,37 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       departamento: {
+<<<<<<< HEAD
         type: Sequelize.INTEGER,
+=======
+        type: Sequelize.STRING,
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
       },
       razonSocial: {
         type: Sequelize.STRING,
       },
       cuit: {
+<<<<<<< HEAD
         type: Sequelize.INTEGER(12),
+=======
+        type: Sequelize.INTEGER,
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
       },
       tipoEmpleador: {
         type: Sequelize.STRING,
       },
       telefono: {
+<<<<<<< HEAD
         type: Sequelize.INTEGER(12),
       },
       /*Tipo-Empresa*/
       areaDeInteres: {
+=======
+        type: Sequelize.INTEGER,
+      },
+      /*Tipo-Empresa*/
+      areaInteres: {
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         type: Sequelize.STRING,
       },
       sitioWeb: {
@@ -76,6 +98,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       tipoTelefono1: {
+<<<<<<< HEAD
         type: Sequelize.STRING,
       },
       telefono1: {
@@ -89,6 +112,18 @@ module.exports = {
       },
       terminosYCondiciones: {
         type: Sequelize.BOOLEAN,
+=======
+        type: Sequelize.INTEGER,
+      },
+      telefono1: {
+        type: Sequelize.INTEGER,
+      },
+      tipoTelefono2: {
+        type: Sequelize.INTEGER,
+      },
+      telefono2: {
+        type: Sequelize.INTEGER,
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
       },
       createdAt: {
         allowNull: false,
--- db/seeders/20210602172034-registro.js
+++ db/seeders/20210602172034-registro.js
@@ -4,19 +4,33 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('registro', [
       {
+<<<<<<< HEAD
         estado: 'pendiente',
         nombreComercial: 'adidas',
         provinciaActual: 'buenos aires',
+=======
+        nombreComercial: 'adidas',
+        provincia: 'buenos aires',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         localidad: 'moron',
         direccion: 'unaDireccion',
         piso: '0',
         codigoPostal: '1001',
+<<<<<<< HEAD
         departamento: '00',
         razonSocial: 'unaRazonSocial1',
         cuit: '142356743',
         tipoEmpleador: 'elTipo1',
         telefono: '9473454',
         areaDeInteres: 'area interesante2',
+=======
+        departamento: 'test1',
+        razonSocial: 'unaRazonSocial1',
+        cuit: '1423',
+        tipoEmpleador: 'elTipo1',
+        telefono: '9474',
+        areaInteres: 'area interesante2',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         sitioWeb: 'ejecutaTuMente.com.es',
         email: 'ejecuta@yahoo.com',
         tipoEmpresa: 'unTipo2',
@@ -25,28 +39,49 @@ module.exports = {
         apellidoRepresentante: 'froste',
         rolRepresentante: 'suRol3',
         emailRepresentante: 'emailrepresentante3@gmail.com',
+<<<<<<< HEAD
         tipoTelefono1: 'fijo',
         telefono1: '0876345',
         tipoTelefono2: 'fijo',
         telefono2: '2355176',
         terminosYCondiciones: false,
+=======
+        tipoTelefono1: '01',
+        telefono1: '0876',
+        tipoTelefono2: '02',
+        telefono2: '2355',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+<<<<<<< HEAD
         estado: 'pendiente',
         nombreComercial: 'puma',
         provinciaActual: 'buenos aires',
+=======
+        nombreComercial: 'puma',
+        provincia: 'buenos aires',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         localidad: 'hurlingham',
         direccion: 'unaDireccion2',
         piso: '0',
         codigoPostal: '1002',
+<<<<<<< HEAD
         departamento: '00',
         razonSocial: 'unaRazonSocial2',
         cuit: '14435658',
         tipoEmpleador: 'elTipo2',
         telefono: '9674443',
         areaDeInteres: 'area interesante',
+=======
+        departamento: 'test2',
+        razonSocial: 'unaRazonSocial2',
+        cuit: '1443',
+        tipoEmpleador: 'elTipo2',
+        telefono: '9674',
+        areaInteres: 'area interesante',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         sitioWeb: 'holaMundo.com.ar',
         email: 'hola@mundo.com',
         tipoEmpresa: 'unTipo',
@@ -55,28 +90,49 @@ module.exports = {
         apellidoRepresentante: 'evans',
         rolRepresentante: 'suRol2',
         emailRepresentante: 'emailrepresentante2@gmail.com',
+<<<<<<< HEAD
         tipoTelefono1: 'fijo',
         telefono1: '34672465',
         tipoTelefono2: 'fijo',
         telefono2: '34672468',
         terminosYCondiciones: false,
+=======
+        tipoTelefono1: '01',
+        telefono1: '3467',
+        tipoTelefono2: '02',
+        telefono2: '3467',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+<<<<<<< HEAD
         estado: 'pendiente',
         nombreComercial: 'LG',
         provinciaActual: 'Formosa',
+=======
+        nombreComercial: 'LG',
+        provincia: 'Formosa',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         localidad: 'formosa',
         direccion: 'unaDireccion3',
         piso: '6',
         codigoPostal: '1201',
+<<<<<<< HEAD
         departamento: '00',
         razonSocial: 'unaRazonSocial3',
         cuit: '6723367',
         tipoEmpleador: 'elTipo3',
         telefono: '968224657',
         areaDeInteres: 'area interesante3',
+=======
+        departamento: 'test3',
+        razonSocial: 'unaRazonSocial3',
+        cuit: '6723',
+        tipoEmpleador: 'elTipo3',
+        telefono: '9682',
+        areaInteres: 'area interesante3',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         sitioWeb: 'reactCodeJS.edu.com',
         email: 'react@microsoft.com',
         tipoEmpresa: 'unTipo3',
@@ -86,11 +142,18 @@ module.exports = {
         apellidoRepresentante: 'blaze',
         rolRepresentante: 'suRol',
         emailRepresentante: 'emailrepresentante1@gmail.com',
+<<<<<<< HEAD
         tipoTelefono1: 'fijo',
         telefono1: '14662356',
         tipoTelefono2: 'fijo',
         telefono2: '93497777',
         terminosYCondiciones: false,
+=======
+        tipoTelefono1: '01',
+        telefono1: '1466',
+        tipoTelefono2: '02',
+        telefono2: '9349',
+>>>>>>> bcfcb54 (paginacion y CRUD registro de datos OK)
         createdAt: new Date(),
         updatedAt: new Date(),
       },
