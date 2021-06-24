3211a7caef195b0a1f734bdd8b29ba34f8a0b914
leonelmperalta <leonelm.peralta@outlook-es>
2021-06-24 11:04:48 -0300

    registro ahora sube datos a la db y logo a google cloud

---
+++ db/migrations/20210624134046-agregado-url-a-registro.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn(
+          'registro',
+          'urlImagen',
+          {
+            type: Sequelize.DataTypes.STRING,
+          },
+          { transaction: t }
+        ),
+      ]);
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn('registro', 'urlImagen', {
+          transaction: t,
+        }),
+      ]);
+    });
+  },
+};
