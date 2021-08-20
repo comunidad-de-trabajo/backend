c223c429526357908d4e377f46f419b883831ad5
kevinmatias120 <acastakev888@gmail.com>
2021-08-20 18:33:14 -0300

    crud usuario hecho

---
+++ db/migrations/20210820192258-create-usuario.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('usuario', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nombreUsuario: {
+        type: Sequelize.STRING,
+      },
+      contrasenia: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('usuario');
+  },
+};
