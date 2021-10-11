14ee2d852c86c0c0386c908f54223c3ff70d502e
leonelmperalta <leonelm.peralta@outlook-es>
2021-10-11 17:39:33 -0300

    agregadas relaciones entre tablas, migraciones, y cuando se crea una oferta se manda el email del usuario para que se relacione la oferta con el usuario

---
+++ db/migrations/20211011190050-foreing-key-usuarioId-en-oferta-laboral.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'OfertaLaboral', // name of Source model
+      'usuarioId', // name of the key we're adding
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'usuario', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'OfertaLaboral', // name of Source model
+      'usuarioId' // key we want to remove
+    );
+  },
+};
